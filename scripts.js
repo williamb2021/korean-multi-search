b0fecfa25d44bfe535639c2571964b3a98b05c2e
WilliamB2021 <bonewj@gmail.com>
2022-01-03 14:32:55 -0800

    Changed order of windows opening so images is first

--- scripts.js
+++ scripts.js
@@ -13,11 +13,12 @@ searchBox.addEventListener('keydown', (e) =>{
     });
 
 let openWindows = searchString => {
+    
     windows.push(window.open(`https://papago.naver.com/?sk=ko&tk=en&st=${searchString}`));
-    windows.push(window.open(`https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=${searchString}`));
     windows.push(window.open(`https://en.wiktionary.org/wiki/${searchString}`));
     windows.push(window.open(`https://www.google.co.kr/search?q=${searchString}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiX4oKPopb1AhWTGjQIHSNZAZwQ_AUoAXoECAEQAw&biw=975&bih=1126`));
     windows.push(window.open(`https://en.dict.naver.com/#/search?query=${searchString}`));
+    windows.push(window.open(`https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=${searchString}`));
 }
 
 let addHistory = searchTerm => {
