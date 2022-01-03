7743d622137bb71165dbbd2c28ece0a873c682ea
WilliamB2021 <bonewj@gmail.com>
2022-01-03 14:05:45 -0800

    Close open windows on new search

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 const searchBox  = document.querySelector(".search");
 const container = document.querySelector(".row");
+let windows = [];
 
 searchBox.addEventListener('keydown', (e) =>{
     let searchString = searchBox.value;
         if (e.which === 13){
+            closeOldWindows();
             openWindows(searchString);
             addHistory(searchString);
             searchBox.value = "";
@@ -11,11 +13,11 @@ searchBox.addEventListener('keydown', (e) =>{
     });
 
 let openWindows = searchString => {
-    window.open(`https://papago.naver.com/?sk=ko&tk=en&st=${searchString}`);
-    window.open(`https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=${searchString}`);
-    window.open(`https://en.wiktionary.org/wiki/${searchString}`);
-    window.open(`https://www.google.co.kr/search?q=${searchString}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiX4oKPopb1AhWTGjQIHSNZAZwQ_AUoAXoECAEQAw&biw=975&bih=1126`);
-    window.open(`https://en.dict.naver.com/#/search?query=${searchString}`);
+    windows.push(window.open(`https://papago.naver.com/?sk=ko&tk=en&st=${searchString}`));
+    windows.push(window.open(`https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=${searchString}`));
+    windows.push(window.open(`https://en.wiktionary.org/wiki/${searchString}`));
+    windows.push(window.open(`https://www.google.co.kr/search?q=${searchString}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiX4oKPopb1AhWTGjQIHSNZAZwQ_AUoAXoECAEQAw&biw=975&bih=1126`));
+    windows.push(window.open(`https://en.dict.naver.com/#/search?query=${searchString}`));
 }
 
 let addHistory = searchTerm => {
@@ -24,8 +26,13 @@ let addHistory = searchTerm => {
     historyButton.textContent = searchTerm;
     container.appendChild(historyButton);
     historyButton.addEventListener('click', (e) => {
+        closeOldWindows();
         openWindows(e.target.innerHTML);
     })
 }
 
-
+let closeOldWindows = () =>{
+    for(var i = 0; i < windows.length; i++){
+        windows[i].close()
+    }
+}
