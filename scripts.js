bea0eef76db2f4b7685a13cc1c0cb8c5ab47b0eb
WilliamB2021 <bonewj@gmail.com>
2022-01-03 11:07:19 -0800

    Added naver endic to search

--- scripts.js
+++ scripts.js
@@ -11,10 +11,11 @@ searchBox.addEventListener('keydown', (e) =>{
     });
 
 let openWindows = searchString => {
-    window.open(`https://papago.naver.com/?sk=ko&tk=en&st=${searchString}`)
-    window.open(`https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=${searchString}`)
-    window.open(`https://en.wiktionary.org/wiki/${searchString}`)
-    window.open(`https://www.google.co.kr/search?q=${searchString}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiX4oKPopb1AhWTGjQIHSNZAZwQ_AUoAXoECAEQAw&biw=975&bih=1126`)
+    window.open(`https://papago.naver.com/?sk=ko&tk=en&st=${searchString}`);
+    window.open(`https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=${searchString}`);
+    window.open(`https://en.wiktionary.org/wiki/${searchString}`);
+    window.open(`https://www.google.co.kr/search?q=${searchString}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiX4oKPopb1AhWTGjQIHSNZAZwQ_AUoAXoECAEQAw&biw=975&bih=1126`);
+    window.open(`https://en.dict.naver.com/#/search?query=${searchString}`);
 }
 
 let addHistory = searchTerm => {
