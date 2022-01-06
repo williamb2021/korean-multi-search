c98f568cee725ffdbd08998ce36603256992e71f
WilliamB2021 <bonewj@gmail.com>
2022-01-05 22:11:26 -0800

    added back in opening windows because it's better for studying

--- scripts.js
+++ scripts.js
@@ -1,19 +1,24 @@
 const searchBox  = document.querySelector(".search");
 const container1 = document.querySelector(".row1");
 const container2 = document.querySelector(".row2");
+let windows = [];
 
 searchBox.addEventListener('keydown', (e) =>{
     let searchString = searchBox.value;
         if (e.which === 13){
+            removeLinks();
             closeOldWindows();
             createLinks(searchString);
+            openWindows(searchString);
             addHistory(searchString);
             searchBox.value = "";
         }
+        
     });
 
 let createLinks = searchString => {
     let links = [];
+
     links.push(`https://papago.naver.com/?sk=ko&tk=en&st=${searchString}`);
     links.push(`https://koreanverb.app/?search=${searchString}`);
     links.push(`https://en.wiktionary.org/wiki/${searchString}`);
@@ -34,6 +39,15 @@ let createLinks = searchString => {
 
 }
 
+let openWindows = searchString => {
+    windows.push(window.open(`https://papago.naver.com/?sk=ko&tk=en&st=${searchString}`));
+    windows.push(window.open(`https://koreanverb.app/?search=${searchString}`));
+    windows.push(window.open(`https://en.wiktionary.org/wiki/${searchString}`));
+    windows.push(window.open(`https://en.dict.naver.com/#/search?query=${searchString}`));
+    windows.push(window.open(`https://www.google.co.kr/search?q=${searchString}&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiX4oKPopb1AhWTGjQIHSNZAZwQ_AUoAXoECAEQAw&biw=975&bih=1126`));
+    windows.push(window.open(`https://search.naver.com/search.naver?sm=tab_hty.top&where=image&query=${searchString}`));   
+}
+
 let addHistory = searchTerm => {
     const historyButton = document.createElement("button");
     historyButton.classList.add("history-button")
@@ -41,20 +55,23 @@ let addHistory = searchTerm => {
     container1.appendChild(historyButton);
     historyButton.addEventListener('click', (e) => {
         closeOldWindows();
+        removeLinks();
         openWindows(e.target.innerHTML);
     })
 }
 
-//need to replace this function with something to remove the links from the page
-// let closeOldWindows = () =>{
-//     for(var i = 0; i < windows.length; i++){
-//         windows[i].close()
-//     }
-// }
 let closeOldWindows = () =>{
+    // for(var i = 0; i < windows.length; i++){
+    //     windows[i].close()
+    // }
+    windows.forEach(window => window.close());
+}
+
+let removeLinks = () =>{
     let links = document.querySelectorAll('.link');     
     let lineBreaks = document.querySelectorAll('br');
     links.forEach(link => link.remove());
     lineBreaks.forEach(lineBreak => lineBreak.remove());
+    
 }
 
